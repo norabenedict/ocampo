d9587bada984f6685d529a629d2b22dc46f704a4
Nora Benedict <ncb3ka@virginia.edu>
2018-07-23 16:16:08 -0400

    updated map coordinates and people

---
+++ merge_people_places_js.js
@@ -0,0 +1,30 @@
+var people = require('./People-2018-07-17.json')
+var places = require('./Place-2018-07-17.json')
+
+let output = []
+
+for (let place of places) {
+    // create a new point
+    let point = {
+        type: "Feature",
+        geometry: {
+            type: "Point",
+            coordinates: [place.longitude, place.latitude]
+        },
+        properties: {
+            name: place.name,
+            popupContent: place.name,
+            affiliatedPeople: []
+        }
+    }
+    // add the affiliated people to place
+    for (let person of people) {
+        if (place.id == person.birthplace || place.id == person.deathplace) {
+            point.properties.affiliatedPeople.push(person.name)
+        }
+    }
+    // add the new point to the list
+    output.push(point)
+}
+
+console.log(JSON.stringify(output, null, 2))
